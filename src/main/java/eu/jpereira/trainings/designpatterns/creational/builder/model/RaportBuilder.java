ca3907c0f8d7d04e2f21862d5ff70db498fb22f5
LabRepository <czyszczonikjakub@gmail.com>
2017-11-20 18:31:34 +0100

    First Commit

---
+++ src/main/java/eu/jpereira/trainings/designpatterns/creational/builder/model/RaportBuilder.java
@@ -0,0 +1,8 @@
+package eu.jpereira.trainings.designpatterns.creational.builder.model;
+
+
+public interface RaportBuilder {
+
+    ReportBody getRaportBody();
+    void buildSaleEntry(SaleEntry saleEntry);
+}
